2b6ebd9fec001f7fc549986428ff2f10f19ab9bb
Rifatx990 <158224402+Rifatx990@users.noreply.github.com>
2025-10-07 16:34:56 +0600

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,9 +1,13 @@
 import threading
-logs = []
+
+LOG_HISTORY = []
+lock = threading.Lock()
 
 def add_log(message):
-    logs.append(message)
-    print(message)
+    with lock:
+        LOG_HISTORY.append(message)
+        print(message)
 
 def get_logs():
-    return logs[-100:]  # last 100 logs
+    with lock:
+        return LOG_HISTORY[-100:]  # last 100 logs
