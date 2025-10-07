6e5ebec793ad4d6c2a504c2913ecc7fa88ee12cb
Rifatx990 <158224402+Rifatx990@users.noreply.github.com>
2025-10-07 18:09:06 +0600

    Update __init__.py

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,8 +1,7 @@
 from .dashboard import bp_dashboard
-from .login_route import bp_login
 from .logs import bp_logs
 from .scheduler_api import bp_scheduler
 from .send_now import bp_send
 
-# Combine all blueprints in a list
-bp_routes = [bp_dashboard, bp_login, bp_logs, bp_scheduler, bp_send]
+# Combine all static blueprints in a list
+bp_routes = [bp_dashboard, bp_logs, bp_scheduler, bp_send]
