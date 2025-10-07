4717fc8f040af92a330318dc98b32a29948f8b91
Rifatx990 <158224402+Rifatx990@users.noreply.github.com>
2025-10-07 16:36:18 +0600

    Update logs.py

--- routes/logs.py
+++ routes/logs.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, jsonify
-from logger import LOG_HISTORY
+from logger import get_logs
 
 bp_logs = Blueprint("logs", __name__)
 
 @bp_logs.route("/logs")
 def logs_route():
-    return jsonify({"logs": LOG_HISTORY})
+    return jsonify({"logs": get_logs()})
