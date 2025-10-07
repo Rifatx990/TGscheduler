330e7f9784ac399dd2df6fbe5ff5cf82c571bd47
Rifatx990 <158224402+Rifatx990@users.noreply.github.com>
2025-10-07 16:52:31 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,10 @@
 import os
-
 from dotenv import load_dotenv
+
 load_dotenv()
 
-API_ID = int(os.getenv("API_ID", 0))
-API_HASH = os.getenv("API_HASH", "")
-SESSION_NAME = "rifat_session"
-SCHEDULE_FILE = "schedule.json"
-TIMEZONE = "Asia/Dhaka"
-PORT = int(os.getenv("PORT", 10000))
-UPLOAD_FOLDER = "uploads"
-
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+SESSION_NAME = os.getenv("SESSION_NAME", "session")
+UPLOAD_FOLDER = os.path.join(os.getcwd(), "uploads")
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
